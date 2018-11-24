7744506e2245fb7926aa6fbe76f623e579eea306
isaac <oseasisaac91@gmail.com>
2018-11-24 12:27:12 -0500

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,28 @@
+class Post
+
+  attr_accessor :author, :title
+
+@@all = []
+
+  def initialize (title)
+    @title = title
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def author_name
+
+    if self.author == nil
+      return nil
+    else
+      return self.author.name
+    end
+
+
+  end
+
+
+end
